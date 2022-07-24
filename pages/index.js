38b3141b95f7882c04e42ab70e6b55d09b4d502d
Devid <99605420+Devid3213@users.noreply.github.com>
2022-07-24 22:41:56 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,21 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-      <html>
+      <!doctype html>
+       <html>
        <head>
          <title>This is Netlify Website</title>
        </head>
        <body>
+       <header>
         <h1>Hello World!</h1>
+       </header>
+       <div>
+        <h1>Hello World!</h1>
+       </div>
+       <footer>
+        Copyright by Devid.
+       </footer>
        </body>
      </html>
   )
